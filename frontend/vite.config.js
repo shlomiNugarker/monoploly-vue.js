9b2068a6e76b5d1de1088bfa559c1f6664f5d61d
Shlomi Nugarker <shlomin1231@gmail.com>
2022-07-22 12:18:37 +0300

    gd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: '/Monoploly-Vue.js/',
   // build: '../backend/public',
   build: {
     chunkSizeWarningLimit: 3600,
